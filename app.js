f03a0a9a540afa5b13bcbb1e2627d76883fdd467
Linet Chepngeno <linetmutai@gmail.com>
2024-01-17 21:59:07 +0300

    update function to display memes on the HTML

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 document.addEventListener('DOMContentLoaded', function (){
     const memeContainer = document.getElementById('meme details');
     const memeList = document.getElementById('memes');
-    //Function to fetch meme details
-    function fetchMemeDetails(memeId){
-        fetch(`http://localhost:3000/data /${memeId}`);
-        .then (response => response.json());
-        .then (data => displayMemeDetails)
-    }   
+//Function to fetch meme details
+    function fetchMemeDetails(memeId) {
+        fetch(`http://localhost:3000/data/${memeId}`)
+            .then(response => response.json())
+            .then(data => displayMemeDetails(data));
+    }
 })
+// Function to display meme details
+function to displayMemeDetails(data) {
+        const memeDetailsHTML =`
+        <h2>${data.name}</h2
+        <img src="${data.url}" alt="${data.name}"
+        `
+       
+        
+}
