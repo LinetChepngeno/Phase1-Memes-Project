2d05ec5c3232a4d409f793682b79c69d8405646a
Linet Chepngeno <linetmutai@gmail.com>
2024-01-17 22:38:54 +0300

    update the meme details to display

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ document.addEventListener('DOMContentLoaded', function (){
     function fetchMemeDetails(memeId) {
         fetch(`http://localhost:3000/data/${memeId}`)
             .then(response => response.json())
-            .then(data => displayMemeDetails(data));
+            .then(data => displayMemeDetails(data))
+            .catch(error => console.error('Error fetching meme details:'))
     }
 })
 // Function to display meme details
-function to displayMemeDetails(data) {
+function displayMemeDetails(data) {
         const memeDetailsHTML =`
-        <h2>${data.name}</h2
-        <img src="${data.url}" alt="${data.name}"
-        `
-       
-        
+        <h2>${data.name}</h2>
+        <img src="${data.url}" alt="${data.name}" style="max-width:100%;">
+        <p>Dimensions: ${data.width} * ${data.height}</p>
+        <p>Box Count: ${data['box_count']}</p>
+        <p>Captions: ${data.captions}</p>
+        `;
+       memeContainer.innerHTML = memeDetailsHTML      
 }
+fetchMemeDetails();
